dcd6045dac5d9b7b1d02c9468f4f325392f2b0ce
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2023-02-03 17:01:46 +0300

    add calculation model by touch button

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class CalculateViewController: UIViewController {
-
+    
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var weightSlider: UISlider!
     
+    var calculatorBrain = CalculatorBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func heightVaueChanged(_ sender: UISlider) {
         let height = String(format: "%.2f", sender.value)
         heightLabel.text = "\(height)m"
@@ -33,8 +34,19 @@ class CalculateViewController: UIViewController {
     
     @IBAction func calculatePressed(_ sender: UIButton) {
         let height = heightSlider.value
-        let weight = weightSlider.value
-        let bmi = weight / (height * height)
+            let weight = weightSlider.value
+
+            calculatorBrain.calculateBMI(height: height, weight: weight)
+            performSegue(withIdentifier: "GoToResultVC", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "GoToResultVC" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+        }
     }
 }
 
